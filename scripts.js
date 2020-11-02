66fc97ad14fef9fd3bd90a675d81c21371954211
keddon21 <selvinmartin1@gmail.com>
2020-11-02 00:39:33 -0500

    Complete project1

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
 /* Add your JavaScript to this file */
+var email = document.getElementById("email");
+
+
+const newLocal = "Thank you! Your email address $email has been added to our mailing list!";
+document.getElementsByClassName("message").innerHTML = newLocal
